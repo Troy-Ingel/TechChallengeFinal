ebed31d6ec2324da77ab66caa3c0cd0ec644eb4d
Jake <jake@Jakes-MacBook-Air.local>
2017-11-07 12:18:48 -0500

    Add geocode router to get coordinates of senior homes to add markers to Google Map

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var express = require('express');
+var seniorCenterRouter = require('./routes/seniorCenters');
+var geocodeRouter = require('./routes/geocode');
 
 const port = process.env.PORT || 8080;
 
 express()
 	.use(express.static('public'))
+	.use('/centers', seniorCenterRouter)
+	.use('/geocode', geocodeRouter)
 	.listen(port);
---
+++ routes/geocode.js
@@ -0,0 +1,16 @@
+var express = require('express')
+var router = express.Router();
+
+var request = require('request');
+
+const apiKey = 'AIzaSyDKWd6bBOs6KH10TcE5729ZTWeUdrIdyLI';
+
+router.get('/', function (req, res){
+	const address = req.query.address;
+	const url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&key=' + apiKey;
+	request.get(url, function(err, response, body){
+		res.json(JSON.parse(body));
+	});
+});
+
+module.exports = router;
