563f9b1d56841789b4f5729936889c3c06e8fb0e
Troy <troy.ing1996@gmail.com>
2017-11-14 16:59:00 -0500

    Mobile layout test 8

--- public/app/controllers/homeController.js
+++ public/app/controllers/homeController.js
@@ -157,6 +157,7 @@ function homeController($window, $location, $scope, $interval, GeoLocationFactor
 						$scope.destination = place.town_city + ', ' + place.st + ' ' + place.tel;
 						$location.path('/directions');
 						$scope.$apply();
+						$scope.activePanel = PANELS.DIRECTIONS;
 					});
 
 					markers.push(marker);
