961a27b6e12d3b85c99dce41eaf1c90964ab170d
Jake <jake@Jakes-MacBook-Air.local>
2017-11-16 11:53:14 -0500

    Cleanup activities route

--- routes/activities.js
+++ routes/activities.js
@@ -1,35 +1,27 @@
 // import modules
 var express = require('express')
 var router = express.Router();
+
 // connect to deployed database
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://heroku_9pk3cb2k:e00cfbbg2i3boo1fagtg6ksmtt@ds257495.mlab.com:57495/heroku_9pk3cb2k";
+const url = "mongodb://heroku_9pk3cb2k:e00cfbbg2i3boo1fagtg6ksmtt@ds257495.mlab.com:57495/heroku_9pk3cb2k";
 
-// endpoint to create the activities collection in the database
-router.get('/create', function(req, res){
-	db.createCollection("activities", function(err, res) {
-		if (err) throw err;
-		console.log("Activities Collection created!");
-		db.close();
-		res.end();
-	});
-})
-// endpoint to post a new activity in the database
-.post('/', function(req, res){
+//endpoint to post a new activity in the database
+router.post('/', function(req, res){
 	MongoClient.connect(url, function(err, db) {
-		if (err) throw err;
+		if(err) throw err;
 
 		db.collection("activities").insertOne(req.body, function(err, res) {
 			if (err) throw err;
-			console.log("New activity inserted", res);
 			db.close();
 		});
 	});
 })
-// endpoint to find all of the activities in the database
+
+//endpoint to find all of the activities in the database
 .get('/', function(req, res){
 	MongoClient.connect(url, function(err, db) {
-		if (err) throw err;
+		if(err) throw err;
 
 		db.collection("activities").find({}).toArray(function(err, result) {
 			if (err) throw err;
@@ -37,7 +29,6 @@ router.get('/create', function(req, res){
 			db.close();
 		});
 	});
-
 });
 
 module.exports = router;
