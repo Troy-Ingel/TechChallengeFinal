e5cb278c701e5ad80ac0178e2f4360f5dfe792ae
Jake <jake@Jakes-MacBook-Air.local>
2017-11-16 09:27:57 -0500

    Fix TV guide

--- public/app/controllers/tvGuideController.js
+++ public/app/controllers/tvGuideController.js
@@ -7,6 +7,7 @@ tvGuideController.$inject = ['$scope', 'TVGuideFactory'];
 function tvGuideController($scope, TVGuideFactory){
 
 	$scope.secondsToDate = secondsToDate;
+	$scope.getTimes = getTimes;
 
 	activate();
 
@@ -20,5 +21,19 @@ function tvGuideController($scope, TVGuideFactory){
 	function secondsToDate(seconds){
 		var date = new Date(seconds * 1000);
 		return ('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2);
+	}
+	function getTimes(){
+		var times = [];
+		var currentHour = (new Date()).getHours();
+
+		for(var i = 0; i < 5; i++){
+			if(i % 2 == 0){
+				times.push(currentHour + ':00');
+			} else{
+				times.push(currentHour + ':30');
+				currentHour++;
+			}
+		}
+		return times;
 	}
 }
--- routes/tv-guide.js
+++ routes/tv-guide.js
@@ -5,8 +5,9 @@ var request = require('request');
 var fs = require('fs');
 
 router.get('/current', function (req, res){
-
-	var url = 'http://mobilelistings.tvguide.com/Listingsweb/ws/rest/schedules/80001.null/start/1510698600/duration/120?ChannelFields=Name%2CFullName%2CNumber%2CSourceId&ScheduleFields=ProgramId%2CEndTime%2CStartTime%2CTitle%2CAiringAttrib%2CCatId&formattype=json&disableChannels=music%2Cppv%2C24hr;';
+	var time = Math.ceil((new Date()).getTime() / 1000);
+	var url = 'http://mobilelistings.tvguide.com/Listingsweb/ws/rest/schedules/80001.null/start/' + time + '/duration/120?ChannelFields=Name%2CFullName%2CNumber%2CSourceId&ScheduleFields=ProgramId%2CEndTime%2CStartTime%2CTitle%2CAiringAttrib%2CCatId&formattype=json&disableChannels=music%2Cppv%2C24hr;';
+	console.log(url);
 	request.get(url, function(err, response, body){
 		res.json(JSON.parse(body));
 	});
